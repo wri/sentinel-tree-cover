51cff9f3269f170a98d8ae0312c4aa08b9aa4590
John Brandt <=>
2020-06-16 14:37:14 -0400

    Clean up predict notebook, unit test tiling function

---
+++ src/models/utils.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+def tile_images(arr: np.ndarray) -> list:
+    """ Converts a 632x632 array to a 289, 16, 16 array
+        
+        Parameters:
+         arr (np.ndaray): (632, 632) float array
+    
+        Returns:
+         images (list): 
+    """
+
+    # Normal
+    images = []
+    for x_offset, cval in enumerate([x for x in range(0, 126, 14)]):
+        for y_offset, rval in enumerate([x for x in range(0, 126, 14)]):
+            base_id = 0
+            subs = arr[:, cval:cval+16, rval:rval+16]
+            images.append(subs)
+            
+    # Upright        
+    for x_offset, cval in enumerate([x for x in range(7,  119, 14)]):
+        for y_offset, rval in enumerate([x for x in range(7, 119, 14)]):
+            base_id = 9*9
+            subs = arr[:, cval:cval+16, rval:rval+16]
+            images.append(subs)
+            
+    # Right
+    for x_offset, cval in enumerate([x for x in range(7, 119, 14)]):
+        for y_offset, rval in enumerate([x for x in range(0, 126, 14)]):
+            base_id = (9*9)+(8*8)
+            subs = arr[:, cval:cval+16, rval:rval+16]
+            images.append(subs)
+            
+    # Up
+    for x_offset, cval in enumerate([x for x in range(0, 119, 14)]):
+        for y_offset, rval in enumerate([x for x in range(7, 119, 14)]):
+            base_id = (9*9)+(8*8)+(9*8)
+            subs = arr[:, cval:cval+16, rval:rval+16]
+            images.append(subs)
+    return images
---
+++ tests/data/test_tiling.py
@@ -0,0 +1,51 @@
+import numpy as numpy
+import os
+import unittest
+
+from src.models.utils import tile_images
+
+class TestTiler(unittest.TestCase):
+
+    def setUp(self):
+        self.size = 9
+        self.size_n = 9*9
+        self.size_ur = 64
+        self.size_u = 72
+        self.size_r = 72
+        self.total = 289
+        self.data = np.random.rand(1, 128, 128)
+
+    def test_tiler(self):
+        preds = np.stack(tile_images(self.data))
+        preds = preds[:, 0, 1:-1, 1:-1]
+        
+        preds_stacked = []
+        for i in range(0, self.size_n, self.size):
+            preds_stacked.append(np.concatenate(preds[i:i + self.size], axis = 1))
+        stacked = np.concatenate(preds_stacked, axis = 0)
+            
+        preds_overlap = []
+        for scene in range(self.size_n, self.size_n+self.size_ur, self.size - 1):
+            to_concat = np.concatenate(preds[scene:scene+ (self.size - 1)], axis = 1)
+            preds_overlap.append(to_concat)    
+        overlapped = np.concatenate(preds_overlap, axis = 0)
+        overlapped = np.pad(overlapped, (7, 7), 'constant', constant_values = 0)
+            
+        preds_up = []
+        for scene in range(self.size_n+self.size_ur, self.size_n+self.size_ur+self.size_r, self.size):
+            to_concat = np.concatenate(preds[scene:scene+self.size], axis = 1)
+            preds_up.append(to_concat)   
+        up = np.concatenate(preds_up, axis = 0)
+        up = np.pad(up, ((7,7), (0,0)), 'constant', constant_values = 0)
+            
+        preds_right = []
+        for scene in range(self.size_n+self.size_ur+self.size_r, self.total, self.size - 1):
+            to_concat = np.concatenate(preds[scene:scene+self.size-1], axis = 1)
+            preds_right.append(to_concat)   
+        right = np.concatenate(preds_right, axis = 0)
+        right = np.pad(right, ((0, 0), (7, 7)), 'constant', constant_values = 0)
+        
+        stacked = stacked + overlapped + right + up
+        stacked = stacked / 4
+
+        self.assertEqual(stacked[7:-7, 7:-7], self.data[0, 8:-8, 8:-8])
