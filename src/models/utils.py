d5ad8f022b503879087a26626a28308ab9e1975d
John Brandt <=>
2020-06-24 12:16:05 -0400

    Fixes boundary artifacts from tile mosaicing

--- src/models/utils.py
+++ src/models/utils.py
@@ -12,29 +12,30 @@ def tile_images(arr: np.ndarray) -> list:
 
     # Normal
     images = []
-    for x_offset, cval in enumerate([x for x in range(0, 126, 14)]):
-        for y_offset, rval in enumerate([x for x in range(0, 126, 14)]):
+    for x_offset, cval in enumerate([x for x in range(0, 140, 14)]):
+        for y_offset, rval in enumerate([x for x in range(0, 140, 14)]):
             base_id = 0
             subs = arr[:, cval:cval+16, rval:rval+16]
             images.append(subs)
             
     # Upright        
-    for x_offset, cval in enumerate([x for x in range(7,  119, 14)]):
-        for y_offset, rval in enumerate([x for x in range(7, 119, 14)]):
+    for x_offset, cval in enumerate([x for x in range(7,  140-7, 14)]):
+        for y_offset, rval in enumerate([x for x in range(7, 140-7, 14)]):
             base_id = 9*9
             subs = arr[:, cval:cval+16, rval:rval+16]
             images.append(subs)
             
     # Right
-    for x_offset, cval in enumerate([x for x in range(7, 119, 14)]):
-        for y_offset, rval in enumerate([x for x in range(0, 126, 14)]):
+    for x_offset, cval in enumerate([x for x in range(7, 140-7, 14)]):
+        for y_offset, rval in enumerate([x for x in range(0, 140, 14)]):
             base_id = (9*9)+(8*8)
             subs = arr[:, cval:cval+16, rval:rval+16]
             images.append(subs)
+
             
     # Up
-    for x_offset, cval in enumerate([x for x in range(0, 119, 14)]):
-        for y_offset, rval in enumerate([x for x in range(7, 119, 14)]):
+    for x_offset, cval in enumerate([x for x in range(0, 140-7, 14)]):
+        for y_offset, rval in enumerate([x for x in range(7, 140-7, 14)]):
             base_id = (9*9)+(8*8)+(9*8)
             subs = arr[:, cval:cval+16, rval:rval+16]
             images.append(subs)
